e86717c627ad84f6df0f8bdacbe572807742b309
James Harrison <james@fasttortoise.co.uk>
2020-02-06 09:48:38 +0000

    Moved to MP4 supporting

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -45,41 +45,10 @@
         //Sets directory variables
         $directory = realpath(__DIR__ . '/../Public/pres/');
         $tmpDir = $directory . '/tmp';
-        $presFile = $directory . '/live.odp';
+        $presFile = $directory . '/live.mp4';
 
         //Moves uploaded file
         $uploadedFile['presFile']->moveTo($presFile);
-
-        //Extracts the ODP to a tmp folder
-        $unzipArchive = new ZipArchive();
-        $unzipArchive->open($presFile);
-        $unzipArchive->extractTo($tmpDir);
-        $unzipArchive->close();
-
-        //Adds the xml values to force full screen
-        $xml = simplexml_load_file($tmpDir . '/content.xml', null, null, 'office', true);
-        $presSettings = $xml->body->presentation->children('urn:oasis:names:tc:opendocument:xmlns:presentation:1.0');
-        $presSettings->addAttribute('presentation:presentation:endless', 'true');
-        $presSettings->addAttribute('presentation:presentation:mouse-visible', 'false');
-        $presSettings->addAttribute('presentation:presentation:pause', 'PT00H00M00S');
-        $presSettings->addAttribute('presentation:presentation:full-screen', 'true');
-        $xml->asXml($tmpDir . '/content.xml');
-
-        //Rezips the document back
-        $zip = new ZipArchive();
-        $zip->open($presFile, ZipArchive::CREATE | ZipArchive::OVERWRITE);
-        $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($tmpDir), RecursiveIteratorIterator::LEAVES_ONLY);
-        foreach ($files as $name => $file) {
-          if (!$file->isDir()) {
-            $filePath = $file->getRealPath();
-            $relativePath = substr($filePath, strlen($tmpDir) + 1);
-            $zip->addFile($filePath, $relativePath);
-          }
-        }
-        $zip->close();
-
-        //Remove all files and folders
-        self::rrmdir($tmpDir);
 
         //Reboot the system
         if(!DEVELOPMENT_MODE) {
